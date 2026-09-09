fab7cab3d52fe529d232857c610df77742b60b22
ahmed3544 <112740118+ahmed3544@users.noreply.github.com>
2026-09-09 16:29:29 +0300

    Fix attendance realtime patch anchor detection

--- scripts/patch_attendance_realtime.ts
+++ scripts/patch_attendance_realtime.ts
@@ -5,15 +5,29 @@ let code = fs.readFileSync(path, 'utf8');
 
 const importLine = 'import { registerAttendanceRealtime } from "./server/attendance-realtime.js";';
 if (!code.includes(importLine)) {
-  const marker = 'import { registerFcmRoutes } from "./server/fcm.js";';
-  if (code.includes(marker)) code = code.replace(marker, `${marker}\n${importLine}`);
-  else throw new Error('attendance realtime import anchor not found');
+  const importAnchors = [
+    'import { registerFcmRoutes } from "./server/fcm.js";',
+    'import * as schema from "./src/db/schema.js";',
+    'import { db } from "./src/db/index.js";'
+  ];
+  const marker = importAnchors.find((x) => code.includes(x));
+  if (marker) code = code.replace(marker, `${marker}\n${importLine}`);
+  else {
+    const firstImportEnd = code.match(/^import[^\n]*;\s*$/m);
+    if (firstImportEnd?.[0]) code = code.replace(firstImportEnd[0], `${firstImportEnd[0]}\n${importLine}`);
+    else throw new Error('attendance realtime import anchor not found');
+  }
 }
 
 if (!code.includes('registerAttendanceRealtime(app);')) {
-  const anchor = 'registerNotificationSystemV2(app);';
-  if (code.includes(anchor)) code = code.replace(anchor, `${anchor}\nregisterAttendanceRealtime(app);`);
-  else {
+  const anchors = [
+    'registerNotificationSystemV2(app);',
+    'registerDeviceSyncV2(app);'
+  ];
+  const marker = anchors.find((x) => code.includes(x));
+  if (marker) {
+    code = code.replace(marker, `${marker}\nregisterAttendanceRealtime(app);`);
+  } else {
     const exportAnchor = 'export default app;';
     if (code.includes(exportAnchor)) code = code.replace(exportAnchor, `registerAttendanceRealtime(app);\n\n${exportAnchor}`);
     else throw new Error('attendance realtime registration anchor not found');
